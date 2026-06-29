d73423c220d5ed462a966ffad3ee7dd1ef1068da
Sudeep <sudeepsudee828@gmail.com>
2026-06-29 14:06:21 +0530

    llm model changed

--- backend/llmserver.js
+++ backend/llmserver.js
@@ -173,7 +173,7 @@ Now answer the user's question.
 }
       ],
       tool_choice: "auto",
-      model: "llama-3.3-70b-versatile",
+      model: "meta-llama/llama-4-scout-17b-16e-instruct",
       temperature: 1,
     });
 
